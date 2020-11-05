8ae54be9a540e954e6b35af39d515d0c64644624
Wolf Reitsamer <reit@htl-donaustadt.at>
2020-11-05 16:03:09 +0100

    Pacman: Ghost-Movement mit Ghost-Asset

---
+++ Pacman/Assets/Scripts/MySpriteAnimator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MySpriteAnimator : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public float speed = 0.25f;
+    
+    private int index;
+    private float timePassed = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+        if (timePassed > speed)
+        {
+            index++;
+            if (index >= sprites.Length)
+                index = 0;
+
+            GetComponent<SpriteRenderer>().sprite = sprites[index];
+            timePassed = 0;
+        }
+    }
+}
---
+++ Pacman/Assets/Scripts/NewGhostController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class NewGhostController : MonoBehaviour
+{
+    public float speed = 3f;
+    
+    private Vector3 currentDirection = Vector2.up;
+
+    private bool performWaypointCheck = true;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos += currentDirection * speed * Time.deltaTime;
+        transform.position = pos;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag.Equals("Waypoint"))
+        {
+            if (performWaypointCheck == false)
+                return;
+            
+            // Spitze - Schaft ---> Vector
+            Vector3 vonMirZumWaypoint = other.transform.position - transform.position;
+            float entfernung = vonMirZumWaypoint.magnitude;
+            if (entfernung < 0.1f)
+            {
+                transform.position = other.transform.position;
+                
+                Waypoint waypoint = other.gameObject.GetComponent<Waypoint>();
+                int numberOfPossibleDirections = waypoint.directions.Length;
+                int index = Random.Range(0, numberOfPossibleDirections);
+                switch (waypoint.directions[index])
+                {
+                    case Waypoint.Direction.Left:
+                        currentDirection = Vector3.left;
+                        break;
+                    case Waypoint.Direction.Right:
+                        currentDirection = Vector3.right;
+                        break;
+                    case Waypoint.Direction.Top:
+                        currentDirection = Vector3.up;
+                        break;
+                    case Waypoint.Direction.Bottom:
+                        currentDirection = Vector3.down;
+                        break;
+                }
+
+                performWaypointCheck = false;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        performWaypointCheck = true;
+    }
+}
